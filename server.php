1c2729df0031bdc7591462f241be46d2eeafd6d2
MyUsernameDenisTrimignogit config --global user.email my@email.comdenis@trimigno.it <denis@trimigno.it>
2023-07-13 16:02:38 +0200

    fine

--- server.php
+++ server.php
@@ -8,7 +8,12 @@ $array = json_decode ($string, true);
 
 if(isset($_POST['todoItem'])) {
 
-    array_push($array, $_POST ['todoItem']) ;
+    $task = [
+        'text' => $_POST ['todoItem'],
+        'done' => false
+    ];
+
+    array_push($array, $task) ;
     $array_encoded = json_encode ($array);
 
     file_put_contents('data/todo_list.json',$array_encoded);
